dfb167f9ccc812218d936cdac92603a82e37ea84
KColagiovanni <KColagiovanni@gmail.com>
2024-09-07 23:47:04 -0700

    Added in activity_id column to the db table.

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ def main():
 
     db = Database()
 
-    # db.drop_table()
-    # db.create_db_table(db.TABLE_NAME)
+    # db.query(db.drop_table)
+    db.create_db_table(db.DATABASE_NAME, db.TABLE_NAME, db.convert_csv_to_df())
 
     # result = db.query(db.commute_data)
     # result = db.query(db.morning_commute)
-    result = db.query(db.afternoon_commute)
-    db.print_commute_specific_query_results(result)
+    # result = db.query(db.afternoon_commute)
+    # db.print_commute_specific_query_results(result)
 
 
 if __name__ == '__main__':
