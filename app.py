70912c3e3d917fad8389e401aed8f4f094e42162
KColagiovanni <KColagiovanni@gmail.com>
2024-09-08 23:11:42 -0700

    Got the database working for the most part. The "Filter Activity" page is showing the activity data. Moving time needs to be adjusted from showing only seconds to showing the time in a hh:mm:ss format.

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///strava_data.db'
 db = SQLAlchemy(app)
 
 class Activity(db.Model):
+
     activity_id = db.Column(db.Integer, primary_key=True)
     activity_name = db.Column(db.String(200), nullable=False)
     start_time = db.Column(db.String(200), nullable=False)
     moving_time = db.Column(db.String(200), nullable=False)
-    distance = db.Column(db.Double, nullable=False)
+    distance = db.Column(db.Double, default=0)
     average_speed = db.Column(db.Double, default=0)
     activity_type = db.Column(db.String(40), nullable=False)
 
@@ -29,7 +30,7 @@ def index():
 
     return render_template('index.html')
 
-@app.route('/activity', methods=['POST', 'GET'])
+@app.route('/activity')  #, methods=['POST', 'GET'])
 def activity():
     '''
     Function and route for the filter activities page.
@@ -38,24 +39,24 @@ def activity():
 
     Returns: Renders the filter_activities.html page.
     '''
-    if request.method == 'POST':
-        activity_content = request.form['content']
-        new_activity = Activity(content=activity_content)
-
-        try:
-            db.session.add(new_activity)
-            db.session.commit()
-            return redirect('/activity')
-        except:
-            return "There was an showing the activity"
-    else:
-        activities = .query.order_by(Activity.activity_id).all()
-        return render_template('activity.html', activities=activities)
+    # if request.method == 'POST':
+    #     activity_content = request.form['content']
+    #     new_activity = Activity(content=activity_content)
+    #
+    #     try:
+    #         db.session.add(new_activity)
+    #         db.session.commit()
+    #         return redirect('/activity')
+    #     except:
+    #         return "There was an showing the activity"
+    # else:
+    activities = Activity.query.order_by(Activity.activity_id).all()
+    return render_template('filter_activities.html', activities=activities)
 
 if __name__ == '__main__':
     app.run(
         # Enabling debug mode will show an interactive traceback and console in the browser when there is an error.
         debug=True,
         host='0.0.0.0',  # Use for local debugging
-        port=5000  # Define the port to use when connecting.
+        port=8000  # Define the port to use when connecting.
     )
