6aaceb0ab8a1b9cf6d0ec60051a6d41e22294066
KColagiovanni <KColagiovanni@gmail.com>
2024-09-10 09:00:45 -0700

    Tryig to modify the movint time and convert if from seconds to hh:mm:ss format.

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ def main():
     db = Database()
 
     # db.query(db.drop_table)
-    db.create_db_table(db.DATABASE_NAME, db.TABLE_NAME, db.convert_csv_to_df())
+    # db.create_db_table(db.DATABASE_NAME, db.TABLE_NAME, db.convert_csv_to_df())
 
-    # result = db.query(db.commute_data)
+    result = db.query(db.commute_data)
     # result = db.query(db.morning_commute)
     # result = db.query(db.afternoon_commute)
-    # db.print_commute_specific_query_results(result)
+    db.print_commute_specific_query_results(result)
 
 
 if __name__ == '__main__':
