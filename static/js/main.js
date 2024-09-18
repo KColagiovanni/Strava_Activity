87cb58a811baf6e6f3a656e61bbcdf944cc055bb
KColagiovanni <KColagiovanni@gmail.com>
2024-09-17 23:40:37 -0700

    Starting to connect the dropdown selection with the filtered results.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 function getDropdownValue() {
     var selectedOption = document.getElementById("dropdown-menu").value;
 //    alert("Selected value: " + selectedOption);  // This will display the selected value
-    document.getElementById("activity-type").innerText = "Showing " + selectedOption + " activities"
+    document.getElementById("activity-type").innerText = "Showing " + selectedOption + " Activities"
     console.log("Showing " + selectedOption + " Activities")
 }
