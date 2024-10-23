679d533c3b59079776b7f9cd46a27d50c7821f5a
KColagiovanni <KColagiovanni@gmail.com>
2024-10-22 23:40:18 -0700

    FInally makign some progress displaying the graphs. Currently only the moving time graph is working, but it seems like it's going to work. Still need to think of a way to only load the graph when the user selects it, not all the graphs when the page loads because it makes the load time really slow.

--- static/js/main.js
+++ static/js/main.js
@@ -1,49 +1,61 @@
-// Used for displaying "Activity Type" output to the page.
-function getDropdownTypeValue() {
-    var selectedOption = document.getElementById("dropdown-menu-type").value;
-    document.getElementById("activity-type").innerText = "Showing " + selectedOption + " Activities"
-    console.log("Showing " + selectedOption + " Activities")
+//// Used for displaying "Activity Type" output to the page.
+//function getDropdownTypeValue() {
+//    var selectedOption = document.getElementById("dropdown-menu-type").value;
+//    document.getElementById("activity-type").innerText = "Showing " + selectedOption + " Activities"
+//    console.log("Showing " + selectedOption + " Activities")
+//}
+//
+//// Used for displaying "Activity Gear" output to the page.
+//function getDropdownTypeValue() {
+//    var selectedOption = document.getElementById("dropdown-menu-gear").value;
+//}
+//
+//const collapseElementList = document.querySelectorAll('.collapse')
+//const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
+//console.log(collapseElementList)
+//
+//var movingTimeRadio = document.getElementById("movingTime").value;
+//var distanceRadio = document.getElementById("distance").value;
+//var avgSpeedRadio = document.getElementById("avgSpeed").value;
+//var maxSpeedRadio = document.getElementById("maxSpeed").value;
+//var elevationGainRadio = document.getElementById("elevationGain").value;
+//
+//function submitGraph() {
+//    console.log("Moving Time: " + movingTimeRadio)
+//    console.log("Distance: " + distanceRadio)
+//    console.log("Average Speed: " + avgSpeedRadio)
+//    console.log("Max Speed: " + maxSpeedRadio)
+//    console.log("Elevation Gain: " + ElevationGainRadio)
+//}
+
+function showGraph(value) {
+    const element = document.getElementById("graph")
+
+    console.log("value is: " + value)
+
+    if (value === "movingTime") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
 }
 
-// Used for displaying "Activity Gear" output to the page.
-function getDropdownTypeValue() {
-    var selectedOption = document.getElementById("dropdown-menu-gear").value;
+function movingTime() {
+    console.log("movingTime has been selected")
 }
 
-const collapseElementList = document.querySelectorAll('.collapse')
-const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
-console.log(collapseElementList)
-
-var movingTimeRadio = document.getElementById("movingTime").value;
-var distanceRadio = document.getElementById("distance").value;
-var avgSpeedRadio = document.getElementById("avgSpeed").value;
-var maxSpeedRadio = document.getElementById("maxSpeed").value;
-var elevationGainRadio = document.getElementById("elevationGain").value;
-
-function submitGraph() {
-    console.log("Moving Time: " + movingTimeRadio)
-    console.log("Distance: " + distanceRadio)
-    console.log("Average Speed: " + avgSpeedRadio)
-    console.log("Max Speed: " + maxSpeedRadio)
-    console.log("Elevation Gain: " + ElevationGainRadio)
+function distance() {
+    console.log("distance has been selected")
 }
 
-//function movingTime() {
-//    console.log("movingTime has been selected")
-//}
-//
-//function distance() {
-//    console.log("distance has been selected")
-//}
-//
-//function avgSpeed() {
-//    console.log("avgSpeed has been selected")
-//}
-//
-//function maxSpeed() {
-//    console.log("maxSpeed has been selected")
-//}
-//
-//function elevationGain() {
-//    console.log("elevationGain has been selected")
-//}
+function avgSpeed() {
+    console.log("avgSpeed has been selected")
+}
+
+function maxSpeed() {
+    console.log("maxSpeed has been selected")
+}
+
+function elevationGain() {
+    console.log("elevationGain has been selected")
+}
