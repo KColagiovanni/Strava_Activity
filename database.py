5340c39f49a988b1463cc9d92e61a5fa8a72e1a7
KColagiovanni <KColagiovanni@gmail.com>
2024-11-11 23:05:00 -0800

    Cleaned up the app.py and database.py files, removing unused lines of code.

--- database.py
+++ database.py
@@ -2,16 +2,12 @@ from datetime import datetime, timedelta
 import pytz
 import pandas as pd
 import sqlite3
-# from tkinter import filedialog as fd
 
 class Database:
 
-    # def __init__(self, upload_directory):
-    #     self.STRAVA_DATA_DIRECTORY = upload_directory
     def __init__(self):
         self.DATABASE_NAME = 'instance/strava_data.db'
         self.TABLE_NAME = 'activity'
-        # STRAVA_DATA_DIRECTORY = fd.askdirectory()
         self.ACTIVITIES_CSV_FILE = 'uploads/activities.csv'
         self.TIMEZONE_OFFSET = 8  # PST offset
         self.KM_TO_MILE = 0.621371
@@ -55,24 +51,10 @@ class Database:
     # ============================== Conversion Functions ==============================
     def convert_csv_to_df(self):
 
-        # print(f'In convert_csv_to_df')
-
-        # Original Strava Activity CSV Location
+        # Strava Activity CSV Location
         try:
             activity_csv_data = pd.read_csv(
-                # self.STRAVA_DATA_DIRECTORY + self.ACTIVITIES_CSV_FILE,
                 self.ACTIVITIES_CSV_FILE
-                # dtype={
-                #     'Activity ID': 'int32',
-                #     'Activity Name': 'string',
-                #     'Activity Type': 'string',
-                #     'Distance': 'float',
-                #     'Commute': 'string',
-                #     'Moving Time': 'float',
-                #     'Max Speed': 'float',
-                #     'Elevation Gain': 'float',
-                #     'Elevation High': 'float',
-                # }
             )
         except FileNotFoundError:
             print(f'No file named {self.ACTIVITIES_CSV_FILE} was found')
@@ -80,28 +62,28 @@ class Database:
 
             # Pandas Data Frame with all the desired data
             desired_data = activity_csv_data[[
-                'Activity ID',  # 0
-                'Activity Date',  # 1
-                'Activity Name',  # 2
-                'Activity Type',  # 3
-                'Distance',  # 4
-                'Commute',  # 5
+                'Activity ID',
+                'Activity Date',
+                'Activity Name',
+                'Activity Type',
+                'Distance',
+                'Commute',
                 'Activity Description',
-                'Activity Gear',  # 6
-                # 'Athlete Weight',  # 7
-                # 'Bike Weight',  # 8
-                'Moving Time',  # 9
-                'Max Speed',  # 10
-                'Elevation Gain',  # 11
-                # 'Elevation Loss',  # 12
-                # 'Elevation Low',  # 13
-                'Elevation High',  # 14
-                # 'Max Grade',  # 15
-                # 'Average Grade',  # 16
-                # 'Average Cadence',  # 17
-                # 'Average Heart Rate',  # 18
-                # 'Average Watts',  # 19
-                # 'Calories'  # 20
+                'Activity Gear',
+                # 'Athlete Weight',
+                # 'Bike Weight',
+                'Moving Time',
+                'Max Speed',
+                'Elevation Gain',
+                # 'Elevation Loss',
+                # 'Elevation Low',
+                'Elevation High',
+                # 'Max Grade',
+                # 'Average Grade',
+                # 'Average Cadence',
+                # 'Average Heart Rate',
+                # 'Average Watts',
+                # 'Calories'
             ]]
 
             # desired_data.fillna({'Commute': False}, inplace=True)
@@ -170,9 +152,6 @@ class Database:
                  'Activity Gear': 'activity_gear'
                  }
             )
-
-            # print(f'desired_data columns is:\n {renamed_column_titles.columns}')
-
             return renamed_column_titles
 
     def format_seconds(self, time):
@@ -198,7 +177,6 @@ class Database:
 
     def convert_df_to_csv(self, df, save_name):
         try:
-            # df.to_csv(f'{self.STRAVA_DATA_DIRECTORY}/{save_name}.csv', header=True, index_label='index')
             df.to_csv(f'{save_name}.csv', header=True, index_label='index')
         except PermissionError:
             print(f'\n!!!!!{save_name} Not Saved!!!!!\nPermission Denied. Make sure the file isn\'t open.\n')
@@ -206,13 +184,9 @@ class Database:
             print(f'CSV File Saved: {save_name}')
 
     def convert_distance(self, row):
-        # print(type(row['Distance']), type(row['Activity Type']))
         if row['Activity Type'] == 'Swim':
-            # print('This is a swim activity')
             return self.convert_meter_to_mile(row['Distance'])
         else:
-            # print('This is not a swim activity')
-            # print(f'row is:\n {row}')
             return self.convert_kilometer_to_mile(row['Distance'])
 
     def convert_kilometer_to_mile(self, km):
@@ -270,73 +244,11 @@ class Database:
     def convert_kg_to_lbs(self, kg):
         return kg * self.KG_TO_LBS
 
-    # # ============================== SQL Queries ==============================
-    # all_query = f'''SELECT * FROM {TABLE_NAME}'''
-    #
-    # column_name_query = f'''PRAGMA table_info({TABLE_NAME})'''
-    #
-
-    #
-    # test_query = f'''SELECT
-    # "Activity Name",
-    #  "Activity Date",
-    #   "Moving Time",
-    #    "Average Speed",
-    #     "Distance",
-    #      "Activity Type",
-    #       "Start Hour"
-    #  FROM {TABLE_NAME}
-    #  WHERE Commute = 0 AND "Activity Type" IS "Ride"'''
-    #
-    # commute_data = f'''SELECT
-    # "Activity Name",
-    #  "Activity Date",
-    #   "Moving Time",
-    #    "Average Speed",
-    #     "Distance",
-    #      "Activity Type",
-    #       "Start Hour"
-    #  FROM {self.TABLE_NAME}
-    #  WHERE Commute = 1 AND "Activity Type" IS "Ride"'''
-    #
-    # morning_commute = f'''SELECT
-    #  "Activity Name",
-    #   "Activity Date",
-    #    "Moving Time",
-    #     "Average Speed",
-    #      "Distance",
-    #       "Activity Type",
-    #        "Start Hour"
-    # FROM {TABLE_NAME}
-    # WHERE (Commute = 1 OR
-    #        "Activity Name" LIKE "%Commute%" OR
-    #         "Activity Name" LIKE "%Morning%") AND
-    #  "Activity Type" IS "Ride" AND "Start Hour" < 10'''
-    #
-    # afternoon_commute = f'''SELECT
-    #  "Activity Name",
-    #   "Activity Date",
-    #    "Moving Time",
-    #     "Average Speed",
-    #      "Distance",
-    #       "Activity Type",
-    #        "Start Hour"
-    # FROM {TABLE_NAME}
-    # WHERE (Commute = 1  OR
-    #        "Activity Name" LIKE "%Commute%" OR
-    #         "Activity Name" LIKE "%Afternoon%") AND
-    #  "Activity Type" IS "Ride" AND "Start Hour" >= 10'''
-    #
-    # activity_date = f'''SELECT "Activity Date"
-    # FROM {TABLE_NAME}
-    #  WHERE Commute = 1'''
-    #
-    # ============================== SQL/Database Functions ==============================
-
     def drop_table(self, db_name):
         connection = sqlite3.connect(db_name)
         c = connection.cursor()
         c.execute(f'''DROP TABLE {self.TABLE_NAME}''')
+        print(f'The {self.TABLE_NAME} table has been dropped.')
         connection.close()
 
     @staticmethod
@@ -353,43 +265,8 @@ class Database:
 
     @staticmethod
     def create_db_table(db_name, db_table_name, data_frame):
-        print(f'data_frame is: {data_frame}')
-        # print(f'dataframe.columns: {data_frame.columns}')
         connection = sqlite3.connect(db_name)
         data_frame.to_sql(
             db_table_name, connection, if_exists='append', index=False
         )
-        print(f'DB Table: {db_table_name} Created Successfully!!')
-
-    def query(self, query_command):
-        print(f'query_command is: {query_command}')
-        try:
-            connection = sqlite3.connect(self.DATABASE_NAME)
-            c = connection.cursor()
-            result = c.execute(query_command).fetchall()
-            connection.close()
-        except Exception as e:
-            # print(e)
-            print(f'Query was NOT successful({e}).')
-        else:
-            print('Query executed successfully!!')
-            return result
-
-    def print_commute_specific_query_results(self, result):
-
-        for i in range(len(result)):
-            print()
-            print(f'Activity Name: {result[i][0]} ({type(result[i][0])})')
-            print(f'Start time: {result[i][1]} ({type(result[i][1])})')
-            print(f'Commute: {result[i][6]} ({type(result[i][6])})')
-            print(f'Moving Time: {result[i][2]} | {self.convert_seconds_to_time_format(result[i][2])} ({type(result[i][2])} | {type(self.convert_seconds_to_time_format(result[i][2]))})')
-            print(f'Distance: {self.convert_kilometer_to_mile(float(result[i][4]))} Miles ({type(self.convert_kilometer_to_mile(float(result[i][4])))})')
-            print(f'Average Speed: {result[i][3]} MPH ({type(result[i][3])})')
-            print(f'Activity Type: {result[i][5]} ({type(result[i][5])})')
-
-
-    @staticmethod
-    def display_db_data(db_name, query_command):
-        connection = sqlite3.connect(db_name)
-        c = connection.cursor()
-        print(c.execute(query_command).fetchall())
+        print(f'The db table "{db_table_name}" was created in the {db_table_name}.db database successfully!!')
