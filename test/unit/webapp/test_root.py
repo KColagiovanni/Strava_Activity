2d8d625f83b2d9d0dcb910a2a40c880772228947
KColagiovanni <KColagiovanni@gmail.com>
2025-01-18 15:34:54 -0800

    Successfully tested the landing page. STarted on the activities tests.

--- test/unit/webapp/test_root.py
+++ test/unit/webapp/test_root.py
@@ -4,5 +4,17 @@ def test_landing(client):
     landing = client.get('/')
     html = landing.data.decode()
 
+    # Check that the Show Activities page is present in the HTML
+    assert '<a class="nav-link" href="/activities">Show Activities</a>' in html
+
+    # Check that the Upload Activities page is present in the HTML
+    assert '<a class="nav-link" href="/upload">Upload Activities</a>' in html
+
+    # Check that the landing/home page is displayed successfully
     assert landing.status_code == 200
 
+def test_activities(client):
+    activity = client.get('/activity/13027315375')
+
+    # Check that the activity page is displayed successfully
+    assert activity.status_code == 200
