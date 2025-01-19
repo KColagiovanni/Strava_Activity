4e6fe633087c456c037ff783dd723e0d426a36bd
KColagiovanni <KColagiovanni@gmail.com>
2025-01-19 15:00:12 -0800

    Setting up a pytest db connection

--- test/unit/webapp/__init__.py
+++ test/unit/webapp/__init__.py
@@ -1,8 +1,22 @@
 import pytest
-from app import app
+from app import db, app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 @pytest.fixture
 def client():
     client = app.test_client()
 
     yield client
+
+@pytest.fixture(scope='session')
+def db_session():
+    engine = create_engine('sqlite:///strava_data.db')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    yield session
+    session.close()
