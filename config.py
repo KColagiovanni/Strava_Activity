96e1ecf58639660fd29bc2708071c6e7c26d44dc
KColagiovanni <KColagiovanni@gmail.com>
2025-01-30 22:43:00 -0800

    Setting up config.py using chatgpt suggestions.

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config:
+    # SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///prod.db')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+
+# Dictionary to map environment names to configuration classes
+config_options = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
