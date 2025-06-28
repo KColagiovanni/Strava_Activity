ab8107d9cb904e3582fbee259dbb34c3a71497df
Kevin Colagiovanni <76464469+KColagiovanni@users.noreply.github.com>
2025-06-27 22:24:41 -0700

    Removed commented out lines in config.py.

--- config.py
+++ config.py
@@ -11,8 +11,6 @@ class Config:
     TIMEZONE_OFFSET = 8  # PST offset
 
     # Variables in routes.py
-    # TARGET_FILENAME = '1297099.fit.gz'
-    # TARGET_FILENAME = r'\d{7,11}(.gpx|.fit.gz|.tcx.gz)'
     TARGET_FILENAME = 'activities.csv'
     DECOMPRESSED_ACTIVITY_FILES_FOLDER = 'decompressed_activity_files'  # Define the directory where decompressed files
     # will be saved.
