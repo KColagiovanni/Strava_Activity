b4f187c896cc51baf1c0cb21e6927b2a042f04ff
Kevin Colagiovanni <76464469+KColagiovanni@users.noreply.github.com>
2026-03-11 22:58:46 -0700

    Updated TODO definitions in test_root.py. Test minor change to test the newly set up CI/CD pipeline.

--- app/routes.py
+++ app/routes.py
@@ -776,7 +776,7 @@ def get_activity_fit_file(activity_id, filepath):
         else:
             activity_dict['power'] = {'x': distance_list, 'y': power_list}
 
-    # To handle: fitparse.utils.FitEOFError: Tried to read 1 bytes from .FIT file but got 0 (Issue #3)
+    # To handle: fitparse.utils.FitEOFError: Tried to read 1 bytes from .FIT file but got 0 (Issue #3 fix)
     except FitEOFError as e:
         print(e)
 
